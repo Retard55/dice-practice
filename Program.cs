2439ea89e3c4b9630887ebcba4bf21ede45ee927
Donkey <christian.26pg@gmail.com>
2026-02-09 09:54:36 +0100

    This is a test commit, for step 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// datatype diceroll (parameters) //
+// codeblock //
+// optional return //
+
+
+
+
+int diceRoll ()
+{
+    Random rnd = new Random ();
+    return rnd.Next (6) + 1;
+}
+
+// run 10 times
+for (int i = 0; i < 10; i += 1)
+{
+    Console.WriteLine (diceRoll()) ;
+}
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
